6c6eeaec51b8426613c0b58071c97fe9c739d331
Robert Sebastian Herlim <robert.herlim@airyrooms.com>
2017-07-29 18:27:07 +0700

    profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -6,9 +6,34 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'profile.html'
 })
 export class ProfilePage {
+  photos : any
+  grids : any
 
   constructor(public navCtrl: NavController) {
+    this.photos = [
+      { url: '/assets/img/gallery/Picss1.jpg', checked: false },
+      { url: '/assets/img/gallery/Picss2.jpg', checked: false },
+      { url: '/assets/img/gallery/Picss3.jpg', checked: false },
+      { url: '/assets/img/gallery/Picss4.jpg', checked: false },
+      { url: '/assets/img/gallery/Picss5.jpg', checked: false },
+      { url: '/assets/img/gallery/Picss6.jpg', checked: false },
+      { url: '/assets/img/gallery/Picss7.jpg', checked: false },
+      { url: '/assets/img/gallery/Picss8.jpg', checked: false },
+      { url: '/assets/img/gallery/Picss9.jpg', checked: false },
+      { url: '/assets/img/gallery/Picss10.jpg', checked: false },
+      { url: '/assets/img/gallery/Picss11.jpg', checked: false },
+      { url: '/assets/img/gallery/Picss12.jpg', checked: false },
+      { url: '/assets/img/gallery/Picss13.jpg', checked: false },
+      { url: '/assets/img/gallery/Picss14.jpg', checked: false },
+      { url: '/assets/img/gallery/Picss15.jpg', checked: false }
+    ]  
 
+    this.grids = [];
+    let tmp = [];
+    for (let i = 0; i < this.photos.length; i++) {
+      tmp.push(this.photos[i])
+      if ((i + 1) % 3 == 0) { this.grids.push(tmp); tmp = [] }
+    }
   }
 
 }
